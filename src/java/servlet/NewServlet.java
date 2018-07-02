22d42dac4d09ae26f8ec7d3c0eca4c6dab1537de
Nurul Firdaus <nurulfirdaus@Nuruls-MacBook-Pro.local>
2018-07-02 23:34:19 +0900

    try

--- src/java/servlet/NewServlet.java
+++ src/java/servlet/NewServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author nurulfirdaus
  */
-@WebServlet(name = "NewServlet", urlPatterns = {"/index.html"})
+@WebServlet(name = "NewServlet", urlPatterns = {"/NewServlet"})
 public class NewServlet extends HttpServlet {
 
     /**
@@ -31,7 +31,6 @@ public class NewServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -46,6 +45,7 @@ public class NewServlet extends HttpServlet {
             out.println("</html>");
         }
     }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
